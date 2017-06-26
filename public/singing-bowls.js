6296823acea644dee69e7f720781cda6ea40a543
nathanallen <thenathanator@gmail.com>
2017-06-26 14:59:21 -0700

    break out js into separate file

---
+++ public/singing-bowls.js
@@ -0,0 +1,130 @@
+function AudioIn() {
+  this.running_min = 0;
+  this.running_max = 0;
+  this.running_avg  = 0;
+  this.samples = [];
+  this.num_samples = 128;
+  this.samples_idx = 0;
+}
+AudioIn.prototype.incoming = function(gain){
+  if(this.samples_idx >= this.num_samples) {
+    this.samples_idx = 0;
+
+    var totals = this.samples.reduce(function(acc, v){
+      return acc + v;
+    }, 0) / this.num_samples;
+
+    this.running_avg = Math.floor( (this.running_avg + totals) / 2 );
+  }
+  this.samples[this.samples_idx] = gain || window.amplitude;
+  this.samples_idx++;
+
+  if (gain < this.running_min) { this.running_min = gain };
+  if (gain > this.running_max) { this.running_max = gain };
+}
+AudioIn.prototype.runningAverage = function(){
+  return this.running_avg;
+}
+AudioIn.prototype.currentAmplitude = function(){
+  return this.samples[this.samples_idx-1];
+}
+AudioIn.prototype.sample = function() {
+  var amp = this.currentAmplitude(),
+      avg = this.runningAverage(),
+      good_sample = true;
+
+  if (amp <= avg/5 ) {
+    console.log("too low", amp)
+    good_sample = false;
+  } else if (amp >= avg*5) {
+    console.log("too high", amp)
+    good_sample = false;
+  }
+
+  return {
+    amplitude: this.currentAmplitude(),
+    avg: this.runningAverage(),
+    max: this.running_max,
+    min: this.running_min,
+    good_sample: good_sample
+  }
+};
+
+
+var graphView = (function(){
+  var els = {};
+  return function(freq, amp){
+    if(!els[freq]) {
+      els[freq] = document.createElement("div");;
+      document.body.appendChild(els[freq]);
+    }
+    els[freq].setAttribute("style", `height:${amp*2}px;`);
+    els[freq].setAttribute("data-freq", freq);
+  }
+}())
+
+function FrequencySampler(osc, audioIn, stepSize=1, offset=440, max=880) {
+  this.freq = stepSize < 0 ? max : offset;
+  this.stepSize = stepSize;
+  this.offset = offset;
+  this.max = max;
+
+  this.loudest = {
+    freq: 0,
+    amp: 0
+  }
+
+  this.intervalID = null;
+
+  this.osc = osc;
+  this.audioIn = audioIn;
+}
+FrequencySampler.prototype.stop = function(){
+  clearInterval(this.intervalID);
+  this.osc.changeFrequency(0);
+}
+FrequencySampler.prototype.play = function(){
+  this.osc.changeFrequency(this.freq);
+  this.intervalID = setInterval(this._nextFreq.bind(this), 10);
+}
+FrequencySampler.prototype._nextFreq = function() {
+  var heard = this.audioIn.sample();
+  if(!heard.good_sample) { return; }
+
+  var amp = heard.amplitude;
+
+  if(amp > this.loudest.amp) {
+    this.loudest.amp = amp;
+    this.loudest.freq = this.freq;
+  }
+
+  setTimeout(graphView(this.freq, amp), 0);
+
+  this.freq += this.stepSize;
+  this.osc.changeFrequency(this.freq);
+
+  if(this.freq > this.max || this.freq < this.offset) {
+    clearInterval(this.intervalID);
+    console.log("loudestFreq:", this.loudest.freq, this.loudest.amp);
+    console.log("averageAmp:", heard.avg);
+
+    this.stepSize = this.stepSize*-1;
+    this.play();
+  }
+}
+
+var osc = new Osc();
+osc.play();
+osc.changeFrequency(0);
+
+var audioIn = new AudioIn();
+
+var frequencySampler = new FrequencySampler(osc, audioIn);
+
+var socket = io();
+socket.on('data', function(data) {
+  audioIn.incoming(+data);
+});
+socket.on('error', function() {
+  console.error(arguments)
+});
